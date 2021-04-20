7423cb11d9680e77a9eeef01274ebeacc27260e4
Georgiy Tonkiy <georgiy.tonkiy@gmail.com>
2021-04-20 12:05:56 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './sass/main.scss';
-import getWeth from './js/getWeather.js';
-// import location from './js/location.js';
-// import renderCurentWether from './js/renderCurentWether.js';
+import './js/getWeather.js';
+// import './js/location.js';
+// import './js/renderCurentWether.js';
 // import './js/quoteForismaticApi';
 // import './js/quoteForismatic';
 // import './js/scrollCountries';
