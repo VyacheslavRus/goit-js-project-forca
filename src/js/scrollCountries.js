a753c93db80f72b045053daae1b7ffce74e8deb9
VyacheslavRus <78310890+VyacheslavRus@users.noreply.github.com>
2021-04-21 20:47:41 +0300

    add shilo

--- src/js/scrollCountries.js
+++ src/js/scrollCountries.js
@@ -1,6 +1,7 @@
-const scrollBtn = document.querySelector('.arrow')
+const scrollBtn = document.querySelector('.favBtn')
 const favoriteBox = document.querySelector('.city_list')
 
 scrollBtn.addEventListener('click', () => {
   favoriteBox.scrollTo(1000,0)
 })
+
